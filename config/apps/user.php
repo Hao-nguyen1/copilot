e33212775c5714e8c6aa148d64a50daeaae1ff5d
Hao-nguyen1 <haocodon03@gmail.com>
2025-02-18 20:38:59 +0700

    up

---
+++ config/apps/user.php
@@ -0,0 +1,17 @@
+<?php 
+return [
+    'index' => [
+    'title' => 'Quản lý người dùng',
+    'table' => 'Danh sách người dùng',
+    ],
+    'edit' => [
+        'title' => 'Chỉnh sửa người dùng',
+    ],
+
+    'create' => [
+        'title' => 'Thêm mới người dùng',
+    ],
+    'delete' => [
+        'title' => 'Xóa người dùng',
+    ]
+];
